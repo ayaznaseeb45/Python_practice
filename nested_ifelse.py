c83615af10faeadf9ba9f3d69451aa99026e5f22
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-01-12 16:24:49 +0500

    nested if else code

---
+++ nested_ifelse.py
@@ -0,0 +1,14 @@
+a = 5
+b = 8
+c = 6
+d = 3
+
+if (a>b):
+    print ("a is greater than b")
+    if(6>2):
+        print("6 is greater than 2")
+    else:
+        print("6 is not greater than 2 ")
+
+else:
+    print ("b is greater than a ")
