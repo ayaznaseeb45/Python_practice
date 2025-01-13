0432b7c278aaeb324767234246dc1130224681f5
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-01-13 11:09:35 +0500

    table

--- forloop.py
+++ forloop.py
@@ -8,8 +8,16 @@
 # for x in range (3):
 #     print("hello ayaz", x)
 
-for number in range(1 , 10):
-    if number % 2 == 0 :
-        print("even number:" , number ,"\n ")
-    else:
-        print("odd number: ", number , "\n")
+
+# even odd in for 
+# for number in range(1 , 10):
+#     if number % 2 == 0 :
+#         print("even number:" , number ,"\n ")
+#     else:
+#         print("odd number: ", number , "\n")
+
+# table in for loop 
+
+for number in range(1, 11):
+    print (f"2 x {number} = {2 * number}")
+
