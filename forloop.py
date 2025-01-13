7dd942171aa7dc156667bbc09a9d756e90cd56de
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-01-13 10:57:02 +0500

    even or in for

--- forloop.py
+++ forloop.py
@@ -1,2 +1,15 @@
-for x in "banana":
-    print(x)
+# for x in "banana":
+#     print(x)
+
+# for x in range(5):
+#     print ("hello Ayaz")
+
+
+# for x in range (3):
+#     print("hello ayaz", x)
+
+for number in range(1 , 10):
+    if number % 2 == 0 :
+        print("even number:" , number ,"\n ")
+    else:
+        print("odd number: ", number , "\n")
