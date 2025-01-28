ba044731e3a63e185c1ae5d3ef00a8220de1c33f
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-01-28 17:53:15 +0500

    for loop

--- forlooop.py
+++ forlooop.py
@@ -1,2 +1,22 @@
-for x in  range(1, 10):
-    print(x++1)
+# even odd using loop
+
+# num1 = int(input("Enter the starting range of the number: "))
+# num2 = int(input("Enter the closing range of the number: "))
+
+# for num in range(num1, num2 + 1): 
+#     if num % 2 == 0:
+#         print(num, "even")
+#     else:
+#         print(num, "odd")
+
+
+# table using loop
+# for num in range(1 , 11):
+#     print(f" 2 x {num} = {2*num}")
+
+# string 
+
+a = "ayaznaseeb"
+
+for char in a :
+    print(f"{char }\n")
