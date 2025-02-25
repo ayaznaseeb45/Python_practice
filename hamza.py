9c91096b25973a10bf4249b953b8d2046a8cbc06
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-02-25 19:54:50 +0500

    hamza khan

---
+++ hamza.py
@@ -0,0 +1,38 @@
+import time
+import os
+
+# for i in range(0, 5):          
+#     time.sleep(1)           
+#     print(f"{i}", end=' ', flush=True)  
+    
+#     time.sleep(2)           
+#     print(f"{4 - i}")       
+
+ 
+import time
+for i in range(1, 4):
+    print(f"about us {i}", end='\r') 
+    time.sleep(2)
+
+
+
+
+
+import time
+import os
+
+# for i in range(0, 5):          
+#     time.sleep(1)           
+#     print(f"{i}", end=' ', flush=True)  
+    
+#     time.sleep(2)           
+#     print(f"{4 - i}")       
+
+ 
+import time
+for i in range(1, 4):
+    print(f"about us {i}", end='\r') 
+    time.sleep(2)
+
+
+
