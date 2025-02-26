b2aa763aeda33879314356e61c21009ae1aeccc0
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-02-26 20:18:32 +0500

    typecasting

---
+++ hamza_new.py
@@ -0,0 +1,24 @@
+# name = "hamza"
+
+# print('h' in name)
+###############
+# arr = "hamzagmail.com"
+
+# print('@' in arr)
+
+##############
+# img= ""
+
+# for i,j,_ in img:
+#     for z in  i:
+#         pass
+
+#     for x in j:
+#         pass
+
+##############
+
+for i in range(5 , 1 , -1):
+    print('*' * i)
+    for x in range(i):
+        print(' ')
---
+++ typecastings.py
@@ -0,0 +1,8 @@
+# a = "30"
+# b ="20"
+# print(int(a) + int(b))
+
+a = input("enter a value:")
+b = input("enter b value:")
+
+print(float(a) + float(b))
