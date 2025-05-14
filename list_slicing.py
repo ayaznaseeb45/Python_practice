e84a140645e07e26a6979ad52a650aacb3c8104c
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-05-14 17:08:46 +0500

    list slicing

--- list_slicing.py
+++ list_slicing.py
@@ -2,6 +2,11 @@
 # print(list1[2:5:1])
 
 
-list1 = [1,2,3,4,5]
 
-print(list1[2:5:])
+# list1 = [1,2,3,4,5]
+# print(list1[-3::])  
+# print(list1[::-1])  
+
+
+list1 = [1,2,3,4,5]
+print(list1[::-1])
