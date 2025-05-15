5cbcfd2b21024edb6c25792556e99fe0a7e820cc
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-05-16 00:36:30 +0500

    tuple operations

---
+++ tuple_operations.py
@@ -0,0 +1,9 @@
+tuple = ("apple", "fruits", "cherry")
+
+# for i in tuple:
+#     print(i)
+
+i = 0
+while i <len(tuple):
+    print(tuple[i])
+    i += 1
---
+++ tuples.py
@@ -0,0 +1,17 @@
+# fruits = ("apple", "banana", "cherry")
+
+# my_tupple = (1,2,3)
+# print(my_tupple)
+# print(type(my_tupple))
+
+# tupple = 1,2,3
+# print(type(tupple))
+
+
+# tpple constructor 
+
+# tupple2 = tuple((1,2,3))
+# print(type(tupple2))
+
+a = ("a")
+print(type(a))
