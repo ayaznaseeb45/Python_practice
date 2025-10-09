2bb6fec8b90efd7a3cdec9043891a455e1dc18ef
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-10-09 15:10:20 +0500

    age problem

--- conditional if else/01_solution.py
+++ conditional if else/01_solution.py
@@ -1,2 +1,14 @@
 # age group customization classify a person age group : Child (<13), teenager (13 - 19), Adult (20 - 59), Senior (60+)
 
+age = int(input("enter your age: "))
+
+if (age < 13):
+    print("you are child ")
+elif age < 20:
+    print("teenager")
+elif age < 60:
+    print("you are adult")
+
+else:
+    print("you are senior")
+
