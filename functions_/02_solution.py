a220254d05251b5d6808d2706bf9b130878778dc
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-10-10 16:42:30 +0500

    multiply func

---
+++ functions_/02_solution.py
@@ -0,0 +1,8 @@
+# write a funtion multiply that multiply string as well 
+
+def multiply_func(num1, num2):
+ 
+    return num1 * num2
+
+result = multiply_func('xx', 'xx')
+print(result)
---
+++ functions_/add.py
@@ -0,0 +1,6 @@
+# write a function that add two numbers 
+
+def add_func(num1, num2):
+    return num1 + num2
+
+print(add_func(2,2))
