f4479bbb22ac93dd843553891a330573e39333a3
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2025-10-10 16:00:55 +0500

    basic return square funtion

--- functions_/basics.py
+++ functions_/basics.py
@@ -1,1 +1,21 @@
 # Functions 
+
+# Write a function to calculate and return the square of a function 
+# NOne wlal case 
+
+
+# def calculate_square(num):
+#     print(num ** 2)
+#     # return num **2
+
+# result = calculate_square(2)
+# print(result)
+
+
+def calculate_square(num):
+    return num **2
+
+result = calculate_square(2)
+print(result)
+
+
