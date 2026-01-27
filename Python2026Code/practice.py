2660150d1843815822fdf18b1d92f4ddcf0ca469
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-01-27 12:14:46 +0500

    total bill divider practice

--- Python2026Code/practice.py
+++ Python2026Code/practice.py
@@ -1,5 +1,14 @@
-a = 15 
-b = 10.5
-c = a + b
+# a = 15 
+# b = 10.5
+# c = a + b
 
-print(type(c))
+# print(type(c))
+
+
+# total_bill = int(input("enter total bill amount: "))
+# total_friends = int(input("enter number of friends "))
+
+# result = total_bill / total_friends
+# print("each will pay: ", result)
+
+
