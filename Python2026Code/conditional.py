174d4db9e76767657fafcf1d7e55766fcd4443ce
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-01-27 15:27:21 +0500

    leap year

--- Python2026Code/conditional.py
+++ Python2026Code/conditional.py
@@ -22,9 +22,55 @@
 
 
 # 3 Write a program to check whether a person is eligible to vote (age ≥ 18).
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-if age >= 18:
-    print("Eligible to vote")
+# if age >= 18:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible to vote")
+
+
+# 🟡 INTERMEDIATE LEVEL
+
+# Write a program to find the largest of two numbers.
+
+# a = int(input("Enter First number: "))
+# b = int(input("Enter Second number: "))
+
+# if (a > b):
+#     print(" 'a' is greater")
+
+# elif(b>a):
+#     print("'b' is greater")
+
+# else:
+#     print("both are equal")
+
+
+
+
+# Largest of Three Numbers
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+# c = int(input("enter third number: "))
+
+# if (a > b and a > c):
+#     print(" 'a' is greater ")
+
+# elif(b > a and b>c ):
+#     print("B is greater")
+
+# else:
+#     print("c is greater")
+
+# print("the greater value is: ", max(a,b,c))
+
+
+
+# Leap year calculator 
+
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap year")
 else:
-    print("Not eligible to vote")
+    print("Not a leap year")    
