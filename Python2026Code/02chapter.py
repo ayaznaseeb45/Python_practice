f2855d294ff1ada51acac90456bef9af1b41712b
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-01-27 11:17:31 +0500

    working of python advance level

---
+++ Python2026Code/02chapter.py
@@ -0,0 +1,9 @@
+# a = 10
+
+# print("memory location of a is :", id(a))
+
+
+# a = input("Enter a number:")
+# print (int(a))
+
+
---
+++ Python2026Code/areaOfCircle.py
@@ -0,0 +1,4 @@
+r = input("enter a numer ")
+pi = 3.14
+area = pi * int(r)**2
+print("area of circle is :", area)
