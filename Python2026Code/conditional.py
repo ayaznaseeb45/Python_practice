9b0387e12cd51267f14ca7fc4bb830cd3bb8c1ca
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-01-27 14:41:56 +0500

    # 3 Write a program to check whether a person is eligible to vote (age ≥ 18).

---
+++ Python2026Code/conditional.py
@@ -0,0 +1,30 @@
+# 1 Write a program to check whether a number is even or odd.
+
+# num = int(input("Enter a number: "))
+
+# if (num % 2 == 0):
+#     print("Even")
+# else:
+#     print(" odd ")
+
+
+# 2 Write a program to check whether a number is positive, negative, or zero.
+
+# num = int(input("Enter a number: "))
+# if (num > 0):
+#     print("Positive: ")
+
+# elif (num < 0):
+#     print("num is negative")
+
+# else:
+#     print("Zero")
+
+
+# 3 Write a program to check whether a person is eligible to vote (age ≥ 18).
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
