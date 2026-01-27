283b2ab0851e448b3d49285b742e8e3903cded52
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-01-27 14:30:03 +0500

    conditional

---
+++ Python2026Code/strings.py
@@ -0,0 +1,24 @@
+# a = "I'm Ayaz\n"
+# b = "Hello 'Ayaz'\n"
+# c = 'Hello "Ayaz"\n'
+# d = '''I'm hello "Ayaz"\n'''
+
+# print(a, b, c, d)
+
+
+# a = "  hello Ayaz  "
+# print("with white spaces   :",a)
+# print("without white spaces:",a.strip())
+
+# print(a[2])
+
+
+a = "green"
+
+mid_value = len(a) // 2   
+
+print("mid value: " , mid_value)
+
+result = a[mid_value]
+
+print("Middle character is:", result)
