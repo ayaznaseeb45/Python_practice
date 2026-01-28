4042fea823594731b3bf705ddc3b09d1ee0f6e29
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-01-28 17:53:06 +0500

    nested loop practice

---
+++ Python2026Code/nestedloop.py
@@ -0,0 +1,36 @@
+# for i in range(3):
+#     for j in range(1, 4):
+#         print(j + 1, end=" ")
+
+
+# for i in range(2, 3):
+#     print(f"Table of {i}")
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i*j}")
+#     print()
+    
+
+
+# for i in range(2, 3):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i*j}")
+
+
+
+
+# num = int(input("Enter a number: "))
+
+# for i in range(num, num + 1):      
+#     for j in range(1, 11):        
+#         print(f"{i} x {j} = {i*j}")
+
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+for i in (list1):
+    for j in (list2):
+        print(f"{i}{j}", end=" ")
+    print()
+      
+
---
+++ Python2026Code/whileloop.py
@@ -0,0 +1,58 @@
+# num = 1
+
+# while(num<5):
+#     num = num + 1
+#     print("still less: ")
+
+
+
+# write a code to print a number from 1 to 10 using while loop 
+# num = 1 
+
+# while num < 11:
+#     print(num)
+#     num = num +1
+
+# write a code to print number from 10 to 1 using while loop 
+
+# i = 10 
+
+# while i >=1:
+#     print(i)
+#     i = i - 1
+
+
+# write a program to print all even numbers from 1 to 50 using while loop
+
+# num = 1
+
+# while num <= 50:
+#     if num % 2 == 0:
+#         print(num)
+#     num = num + 1
+
+
+# write a program to print all odd numbers from 1 to 50 using while loop
+
+# num = 1 
+
+# while num <=50:
+#     if num % 2 !=0:
+#         print(num)
+#     num = num + 1
+
+# write a program to calculate the sum of first 10 natural numbers using while loop
+
+# num = 1
+# sum = 0           
+# while num <= 10:
+#     sum = sum + num   
+#     num = num + 1
+#     print(sum)
+# print("The sum of first 10 natural numbers is:", sum)
+
+# write a program to print a pattern using while loop
+# i = 0
+# while i < 5:
+#     i += 1
+#     print("*" * i)
