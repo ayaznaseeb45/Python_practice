8e93601d8313326db432eac8b37ebdb0d784fa66
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-02-04 17:46:54 +0500

    check specific word is presend

--- Python2026Code/filing.py
+++ Python2026Code/filing.py
@@ -1,3 +1,21 @@
-file = open("dummy.txt", "r")
-data = file.read()
-print(f"data of file is: \n\n",data)
+# file = open("dummy.txt", "r")
+# data = file.read()
+# print(f"data of file is: \n\n",data)
+
+
+file = open("certificate.txt", "r")
+data = file.read().lower()
+file.close()
+
+count = 0
+
+for word in data.split():
+    print(word)
+    if word == "alive":
+        count += 1
+
+if count > 0:
+    print("Yes, 'alive' is present in the file")
+    print(f"'alive' appeared {count} time(s) in the file")
+else:
+    print("'alive' is not available in the file")
