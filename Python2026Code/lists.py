faa96d2cacc5441bd7442d0a94c7d74a0a24ccc9
Ayaz Naseeb <140901201+ayaznaseeb45@users.noreply.github.com>
2026-04-11 10:38:26 +0500

    practicing for Netsol test

--- Python2026Code/lists.py
+++ Python2026Code/lists.py
@@ -1,23 +1,23 @@
-# list1 = [10, 20, 30, 40, 50]
+list1 = [10, 20, 30, 40, 50]
 # print("Original List:", list1)
 
 # list1[1] = 25
 # print("Modified List:", list1)
 
-# print("\n slicing", list1[1:3])
+print("\n slicing", list1[1:3])
 
 
 # list1 = [10, 20, 30, 40, 50]
 
 # list1.append(45)
 # print(list1)
-# list1.sort()
-# print(list1)
+# # list1.sort()
+# # print(list1)
 
 
-fruits = []
-for i in range(3):
-    z = i 
-    a = input("Enter name fruit name:")
-    fruits.append(a)
-print(fruits)
+# fruits = []
+# for i in range(3):
+#     # z = i 
+#     a = input("Enter name fruit name:")
+#     fruits.append(a)
+# print(fruits)
---
+++ Python2026Code/newpractice.py
@@ -0,0 +1,45 @@
+# # values = []
+
+# # for i in range(5):
+# #     user_input =  input("enter a value ")
+# #     values.append(user_input)
+
+# # print(values)
+# # print(reversed(values))
+
+
+# student = {
+#     "name" : "Ayaz",
+#     "age" : 25, 
+    
+# }
+# # students["name"] = "xyz"
+# # students["city"] = "Lahore"
+
+# # print(students)
+
+# # del students["age"]
+# # print(students)
+
+
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+
+# print("name" in student.keys())
+
+# print("salary" in student.keys())
+
+
+
+student = {
+    "name": "Ali",
+    "age": 20,
+    "city": "Lahore"
+}
+
+# for key, values in student.items():
+#     print(f"key '{key}' has value '{values}'")
+
+# print(student.items())
+
